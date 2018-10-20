d0cccada9ff3e96e8016b684de782ad5e5d963b2
Dan Bikle <danbikle@outlook.com>
2018-10-19 23:25:59 -0700

    more

--- script/webserver.bash
+++ script/webserver.bash
@@ -4,7 +4,7 @@
 
 cd ${HOME}/repos/haml4us
 
-# I should start a rails server on all interfaces on port 33000:
+# I should start a rails server on all interfaces on port 33005:
 bin/rails server -b 0.0.0.0 -p 33005
 
 exit
